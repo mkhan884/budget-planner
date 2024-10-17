a7b54d0931321f95bbcb815b4de4dcd81f018fe3
Muhammad Khan <mkwestern1@gmail.com>
2024-10-17 15:36:51 +0400

    removing primevue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  
+  <div>
+    <Sidebar />
+  </div>
 </template>
 
-<script setup>
+<script>
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+  },
+}
 </script>
 
-<style scoped>
-
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+  #app {
+    font-family: 'Montserrat', sans-serif !important;
+  }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
-
 const app = createApp(App)
-
+import router from './router'
 app.use(router)
 
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
 
-app.use(vuetify).mount('#app')   
+app.mount('#app')
